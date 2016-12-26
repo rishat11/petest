15d4602beb8ebd517565e1994b377f02f17006e2
Rishat Sadykov <r.sadickov2010@yandex.ru>
2016-12-26 23:26:49 +0300

    Added Version2.0

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,33 +29,48 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F);
-            this.button1.Location = new System.Drawing.Point(154, 131);
+            this.button1.Location = new System.Drawing.Point(173, 130);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(397, 161);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Version1.0";
+            this.button1.Text = "Version2.0";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F);
+            this.checkBox1.Location = new System.Drawing.Point(173, 49);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(178, 50);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Upgade";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(710, 376);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
